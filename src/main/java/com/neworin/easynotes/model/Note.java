53d7537c714ef8a3a05a7901cd71beff6123d710
NewOrin <neworin@163.com>
2017-04-30 18:04:21 +0800

    添加笔记model

---
+++ src/main/java/com/neworin/easynotes/model/Note.java
@@ -0,0 +1,85 @@
+package com.neworin.easynotes.model;
+
+import java.util.Date;
+
+public class Note {
+    private Long id;
+
+    private Long notebookId;
+
+    private String title;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+    private Integer isDelete;
+
+    private Integer status;
+
+    private String content;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getNotebookId() {
+        return notebookId;
+    }
+
+    public void setNotebookId(Long notebookId) {
+        this.notebookId = notebookId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title == null ? null : title.trim();
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public Integer getIsDelete() {
+        return isDelete;
+    }
+
+    public void setIsDelete(Integer isDelete) {
+        this.isDelete = isDelete;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content == null ? null : content.trim();
+    }
+}
